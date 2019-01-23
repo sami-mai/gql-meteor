1241f1275b7824e16fa6ec86ef98c0236319b8eb
EIT-sami-mai <samirah.maison@meltwater.org>
2019-01-23 07:02:32 +0000

    queries for testing the playground

--- server/main.js
+++ server/main.js
@@ -12,6 +12,31 @@ import resolvers from '../imports/api/resolvers'
 
 import { Views } from '../imports/api/tasks'
 
+Meteor.startup(() => {
+  // create mock data with a seed, so we always get the same
+  // modify the mock data creation to also create some views:
+  casual.seed(123);
+  db.sync({ force: true }).then(() => {
+    _.times(10, () => {
+      return AuthorModel.create({
+        firstName: casual.first_name,
+        lastName: casual.last_name,
+      }).then((author) => {
+        return author.createPost({
+          title: `A post by ${author.firstName}`,
+          text: casual.sentences(3),
+        }).then((post) => { // <- the new part starts here
+          // create some View mocks
+          return Views.rawCollection().update(
+            { postId: post.id },
+            { views: casual.integer(0, 100), postId: post.id },
+            { upsert: true })
+        })
+      })
+    })
+  })
+})
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
